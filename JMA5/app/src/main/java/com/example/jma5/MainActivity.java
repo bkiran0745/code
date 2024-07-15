1a6482988361dfa22d3a9ae87b7993bce084d77f
B Kiran <venukiran601@gmail.com>
2024-07-15 13:08:40 +0530

    Adding JAVA code

---
+++ JMA5/app/src/main/java/com/example/jma5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.jma5;
+//package com.example.radiogroupdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton radioButton = findViewById(selectedId);
+                textView.setText("Selected Gender: " + radioButton.getText());
+            }
+        });
+    }
+}
